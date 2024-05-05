507949154e735c526df094918d94a8a8c61c6957
Sergio <sergiobulankin@gmail.com>
2024-05-05 16:10:32 +0300

    start progect

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+"use strict"
+
+// alert("hello");
+
+// const result = confirm("ты тут?"); 
+
+// const answer = prompt("Тебе есть 18?", "19");
+
+{
+const userName = prompt("Как тебя зовут?");
+
+alert(`Привет, ${userName}`);
+}
