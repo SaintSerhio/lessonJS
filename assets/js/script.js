401f222edb0a7b651099fcb7d23142214ce4f709
Sergio <sergiobulankin@gmail.com>
2024-05-05 18:06:13 +0300

    upgrade js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,27 @@
 
 // const answer = prompt("Тебе есть 18?", "19");
 
-{
+/***{
 const userName = prompt("Как тебя зовут?");
 
 alert(`Привет, ${userName}`);
+}***/
+
+/***
+const  num = 50;
+switch (num) {
+    case 49:
+        console.log("Error");
+        break;
+    case 100:
+        console.log("Is not");
+        break;
+    case 50:
+        console.log("Oh yee");
+        break;
+    default:
+        console.log("It`s not number 50");
+        break;
 }
+***/
+
